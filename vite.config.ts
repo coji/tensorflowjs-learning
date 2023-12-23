dbc402784346936cda6a02a4b14d53d1229e8e6f
coji <coji@techtalk.jp>
2023-12-23 20:52:12 +0900

    Add new routes and components

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     remix({
-      ignoredRouteFiles: ['**/.*'],
+      ignoredRouteFiles: ['**/*'],
+      routes: async (defineRoutes) => flatRoutes('routes', defineRoutes, {}),
     }),
     tsconfigPaths(),
   ],
