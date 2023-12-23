5574dfa0f16b86b4db5b43321991d854798a50ef
coji <coji@techtalk.jp>
2023-12-23 21:20:27 +0900

    Add remixDevTools plugin and update build and dev scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { remixDevTools } from 'remix-development-tools/vite'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    remixDevTools(),
     remix({
       ignoredRouteFiles: ['**/*'],
       routes: async (defineRoutes) => flatRoutes('routes', defineRoutes, {}),
